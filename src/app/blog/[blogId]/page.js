b290b8a5c3726f70df6b1eed0247c3c019850ca4
developer <45854084+webbninja2@users.noreply.github.com>
2023-08-02 14:33:57 +0530

    add routing

---
+++ src/app/blog/[blogId]/page.js
@@ -0,0 +1,13 @@
+import Navbar from "@/app/components/navbar";
+import React from "react";
+
+const SingleBlog = () => {
+  return (
+    <>
+      <Navbar />
+      <h1>Signle blog page</h1>
+    </>
+  );
+};
+
+export default SingleBlog;
